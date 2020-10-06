e774483a4ef8400d3e905daadc502653c2c218f2
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2020-10-06 22:22:10 +0800

    -

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import NavigatePage from './NavigatePage'
 
 class HomePage extends React.Component {
     render() {
-        return{
-            <div>aaa</div>
-        }
+        return(
+            
+            <NavigatePage/>
+        )
     }
 }
 
---
+++ src/pages/NavigatePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class NavigatePage extends React.Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default NavigatePage;
