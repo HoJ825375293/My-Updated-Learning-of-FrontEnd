a86fe353daec41b687e143318e1762571b609da6
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2020-10-06 22:15:47 +0800

    -

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './pages/HomePage'
+import HomePage from './pages/HomePage';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<HomePage /> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
