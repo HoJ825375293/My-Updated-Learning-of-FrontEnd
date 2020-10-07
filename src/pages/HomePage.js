c204bbd35c5b1e7a76ba56ce0a202e9095f9bc7d
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2020-10-07 15:16:08 +0800

    -

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Parallax from './Parallax';
 import '../css/HomePage.css'
+import NavigateTool from './NavigateTool';
 
 class HomePage extends React.Component {
     render() {
         return(
-            <Parallax/>
+            <div>
+                <NavigateTool/>
+                <Parallax/>
+            </div>
         )
     }
 }
---
+++ src/pages/NavigateTool.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NavigateTool extends React.Component {
+    render() {
+        return(
+            <div style ={{height: 1000 }}>aa</div>
+        )
+    }
+}
+
+export default NavigateTool;
