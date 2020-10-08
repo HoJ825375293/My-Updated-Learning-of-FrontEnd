f4bfce5a7cf971339830fb81c0a746b8f0e4d086
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2020-10-08 11:36:34 +0800

    -

--- src/pages/NavigateTool.js
+++ src/pages/NavigateTool.js
@@ -8,7 +8,7 @@ class NavigateTool extends React.Component {
                 <a href="/" className="logo">GoldenExp</a>
                 <ul className="nav">
                     <li><a href="/">Home</a></li>
-                    <li><a href="/">Music</a></li>
+                    <li><a href="/music">Music</a></li>
                     <li><a href="/">Blog</a></li>
                     <li><a href="/">Vedio</a></li>
                     <li><a href="/">Me</a></li>
